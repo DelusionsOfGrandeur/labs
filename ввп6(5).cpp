21c23d3def83c3ef7b38bcaf2af4b9f1383e8e01
DelusionsOfGrandeur <71599221+DelusionsOfGrandeur@users.noreply.github.com>
2020-10-14 11:37:30 +0300

    Update ввп6(5).cpp

--- ввп6(5).cpp
+++ ввп6(5).cpp
@@ -6,7 +6,7 @@ int main()
 {
 	setlocale(LC_ALL, "Russian");
 	int x, y;
-	printf("введите значение x\n");
+	printf("РІРІРµРґРёС‚Рµ Р·РЅР°С‡РµРЅРёРµ x\n");
 	scanf("%d", &x);
 	y = 4*(sqrt(x-3)*sqrt(x-3)*sqrt(x-3)) - 7*sqrt(x-3) + 2;
 	printf("y = " "%d\n", y);
